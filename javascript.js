c8cb8629d3313944c65a7410452009b21485e321
DanielLou <danielderudder@hotmail.com>
2022-05-09 13:02:38 +0200

    Add borders, resize buttons, display and update 'picks'

--- javascript.js
+++ javascript.js
@@ -24,46 +24,58 @@ function playRound(playerSelection, computerSelection) {
     let resultMessage;
     if (score[0] < 5 && score[1] < 5) {
         function winRound() {
+            playerPick.textContent = playerSelection.toUpperCase();
+            compPick.textContent = computerSelection.toUpperCase();
             message.textContent = resultMessage;
             message.classList.add('update-message');
             score[0] += 1;
             scorePlayer.classList.add('update-score');
+            playerPick.classList.add('update-pick');
+            compPick.classList.add('update-pick');
         };
         function loseRound() {
+            playerPick.textContent = playerSelection.toUpperCase();
+            compPick.textContent = computerSelection.toUpperCase();
             message.textContent = resultMessage;
             message.classList.add('update-message');
             score[1] += 1;
             scoreComputer.classList.add('update-score');
+            playerPick.classList.add('update-pick');
+            compPick.classList.add('update-pick');
         };
         function drawRound() {
+            playerPick.textContent = playerSelection.toUpperCase();
+            compPick.textContent = computerSelection.toUpperCase();
             message.textContent = resultMessage;
             message.classList.add('update-message');
+            playerPick.classList.add('update-pick');
+            compPick.classList.add('update-pick');
         }
-        if (playerSelection === 1 && computerSelection === 'paper') {
+        if (playerSelection === 'rock' && computerSelection === 'paper') {
             resultMessage = "YOU LOSE! The computer's Paper beats your Rock.";
             loseRound();
-        } else if (playerSelection === 1 && computerSelection === 'scissors') {
+        } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
             resultMessage = "YOU WIN! Your Rock beats the computer's Scissors.";
             winRound();
-        } else if (playerSelection === 2 && computerSelection === 'rock') {
+        } else if (playerSelection === 'paper' && computerSelection === 'rock') {
             resultMessage = "YOU WIN! Your Paper beats the computer's Rock.";
             winRound();
-        } else if (playerSelection === 2 && computerSelection === 'scissors') {
+        } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
             resultMessage = "YOU LOSE! The computer's Scissors beats your Paper.";
             loseRound();
-        } else if (playerSelection === 3 && computerSelection === 'rock') {
+        } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
             resultMessage = "YOU LOSE! The computer's Rock beats your Scissors.";
             loseRound();
-        } else if (playerSelection === 3 && computerSelection === 'paper') {
+        } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
             resultMessage = "YOU WIN! Your Scissors beats the computer's Paper.";
             winRound();
-        } else if (playerSelection === 1 && computerSelection === 'rock') {
+        } else if (playerSelection === 'rock' && computerSelection === 'rock') {
             resultMessage = "IT\'S A DRAW! Both you and the computer chose Rock.";
             drawRound();
-        } else if (playerSelection === 2 && computerSelection === 'paper') {
+        } else if (playerSelection === 'paper' && computerSelection === 'paper') {
             resultMessage = "IT\'S A DRAW! Both you and the computer chose Paper.";
             drawRound();
-        } else if (playerSelection === 3 && computerSelection === 'scissors') {
+        } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
             resultMessage = "IT\'S A DRAW! Both you and the computer chose Scissors."
             drawRound();
         }};
@@ -120,17 +132,51 @@ scoreboardContainer.style.marginTop = '25px';
 containerOutput.appendChild(scoreboardContainer);
 
 const scoreboardLeft = document.createElement('div');
+scoreboardLeft.classList.add('scoreboard', 'content');
 scoreboardContainer.appendChild(scoreboardLeft);
 
 const scoreboardRight = document.createElement('div');
+scoreboardRight.classList.add('scoreboard', 'content');
 scoreboardContainer.appendChild(scoreboardRight);
 
+const playerHeader = document.createElement('div');
+playerHeader.classList.add('scoreboard', 'header');
+playerHeader.textContent = 'PLAYER'
+scoreboardLeft.appendChild(playerHeader);
+
+const compHeader = document.createElement('div');
+compHeader.classList.add('scoreboard', 'header');
+compHeader.textContent = 'COMPUTER';
+scoreboardRight.appendChild(compHeader);
+
+const playerPickTitle = document.createElement('div');
+playerPickTitle.textContent = "Pick";
+playerPickTitle.classList.add('scoreboard', 'sec-header');
+scoreboardLeft.appendChild(playerPickTitle);
+
+const compPickTitle = document.createElement('div');
+compPickTitle.textContent = "Pick";
+compPickTitle.classList.add('scoreboard', 'sec-header');
+scoreboardRight.appendChild(compPickTitle);
+
+const playerPick = document.createElement('div');
+playerPick.textContent = "Click on a button";
+playerPick.classList.add('score', 'pick')
+scoreboardLeft.appendChild(playerPick);
+
+const compPick = document.createElement('div');
+compPick.textContent = "Click on a button";
+compPick.classList.add('score', 'pick')
+scoreboardRight.appendChild(compPick);
+
 const scoreTitlePlayer = document.createElement('div');
-scoreTitlePlayer.textContent = "YOUR SCORE";
+scoreTitlePlayer.textContent = "Score";
+scoreTitlePlayer.classList.add('scoreboard', 'sec-header');
 scoreboardLeft.appendChild(scoreTitlePlayer);
 
 const scoreTitleComputer = document.createElement('div');
-scoreTitleComputer.textContent = "SCORE COMPUTER";
+scoreTitleComputer.textContent = "Score";
+scoreTitleComputer.classList.add('scoreboard', 'sec-header');
 scoreboardRight.appendChild(scoreTitleComputer);
 
 const scorePlayer = document.createElement('div');
@@ -152,19 +198,19 @@ document.body.appendChild(containerButton);
 
 const button1 = document.createElement('button');
 button1.classList.add('button');
-button1.setAttribute('id', '1')
+button1.setAttribute('id', 'rock')
 button1.textContent = "ROCK"
 containerButton.appendChild(button1);
 
 const button2 = document.createElement('button');
 button2.classList.add('button');
-button2.setAttribute('id', '2');
+button2.setAttribute('id', 'paper');
 button2.textContent = "PAPER"
 containerButton.appendChild(button2);
 
 const button3 = document.createElement('button');
 button3.classList.add('button');
-button3.setAttribute('id', '3')
+button3.setAttribute('id', 'scissors')
 button3.textContent = "SCISSORS"
 containerButton.appendChild(button3);
 
@@ -173,7 +219,7 @@ endMessage.classList.add('end');
 document.body.appendChild(endMessage);
 
 function moreRounds(e) {
-    const buttonId = Number(e.target.id);
+    const buttonId = e.target.id;
     const clickRound = playRound(buttonId, computerPlay());
 };
 
@@ -188,6 +234,21 @@ function removeTransitionScore(e) {
 
 const scores = document.querySelectorAll('.num');
 scores.forEach(score => score.addEventListener('transitionend', removeTransitionScore));
+
+function removeTransitionScore(e) {
+    if (e.propertyName === 'transform') {
+        this.classList.remove('update-score');
+    };
+}
+
+const picks = document.querySelectorAll('.pick');
+picks.forEach(pick => pick.addEventListener('transitionend', removeTransitionPick));
+
+function removeTransitionPick(e) {
+    if (e.propertyName === "transform") {
+        this.classList.remove('update-pick');
+    };
+}
 
 function removeTransitionMessage(e) {
     if (e.propertyName === "transform") {
